67d7569fcaad99bfd80f426ce91a523a16235603
Tamby Mampionona <theriharson@email.com>
2024-02-17 12:19:08 +0300

    Recuperation des tout les titles des livres

--- main.py
+++ main.py
@@ -3,20 +3,11 @@ from pprint import pprint
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://books.toscrape.com/"
-response = requests.get(url)
-
-with open('index.html', "w") as f:
-    f.write(response.text)
-
 with open('index.html', 'r') as f:
     html = f.read()
 
 soup = BeautifulSoup(html, 'html.parser')
-aside = soup.find('div', class_="side_categories")
-categories_div = aside.find("ul").find('li').find('ul')
-categories = [child.text.strip() for child in categories_div.children if child.name]
-
-images = soup.find('section').find_all('img')
-for image in images:
-    print(image['src'])
+articles = soup.find_all('article', class_='product_pod')
+titles_tags = soup.find_all('a', title=True)
+titles = [a['title'] for a in titles_tags]
+pprint(titles)
