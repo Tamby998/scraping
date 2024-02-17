2338242d4f9dadce02d94972a2d1bcddffbf524b
Tamby Mampionona <theriharson@email.com>
2024-02-17 12:07:58 +0300

    Une autre methode pour recuperer les listes des images (utilisant un fichier index.html)

--- main.py
+++ main.py
@@ -6,7 +6,13 @@ from bs4 import BeautifulSoup
 url = "https://books.toscrape.com/"
 response = requests.get(url)
 
-soup = BeautifulSoup(response.text, 'html.parser')
+with open('index.html', "w") as f:
+    f.write(response.text)
+
+with open('index.html', 'r') as f:
+    html = f.read()
+
+soup = BeautifulSoup(html, 'html.parser')
 aside = soup.find('div', class_="side_categories")
 categories_div = aside.find("ul").find('li').find('ul')
 categories = [child.text.strip() for child in categories_div.children if child.name]
