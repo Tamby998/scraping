a4ff7b003d8a033b7e9f2687f38c6ec69ab62895
Tamby Mampionona <theriharson@email.com>
2024-02-17 12:02:31 +0300

    Recuperation de tout les images des livres

--- main.py
+++ main.py
@@ -10,5 +10,7 @@ soup = BeautifulSoup(response.text, 'html.parser')
 aside = soup.find('div', class_="side_categories")
 categories_div = aside.find("ul").find('li').find('ul')
 categories = [child.text.strip() for child in categories_div.children if child.name]
-pprint(categories)
 
+images = soup.find('section').find_all('img')
+for image in images:
+    print(image['src'])
