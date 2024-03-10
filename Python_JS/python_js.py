bdad4725a462bcce0b64c6279a291b190d67210d
Tamby Mampionona <theriharson@email.com>
2024-03-10 11:36:27 +0300

    Utilisation du playwright

--- Python_JS/python_js.py
+++ Python_JS/python_js.py
@@ -1,7 +1,15 @@
+from bs4 import BeautifulSoup
 from playwright.sync_api import sync_playwright
 
 with sync_playwright() as playwright:
     browser = playwright.chromium.launch(headless=False)
     page = browser.new_page()
     page.goto("https://www.docstring.fr/scraping/")
-    page.pause()
+
+    page.locator("css=#get-secrets-books").click()
+    page.wait_for_timeout(2000)
+    html = page.content()
+    soup = BeautifulSoup(html, "html.parser")
+    for titre in soup.select("h2"):
+        print(titre.text)
+    browser.close()
